b5e95c62aa03c67c43a18d0d4ef58387c14c623c
anower00 <anower.hasan@gmail.com>
2020-05-28 11:23:57 +0600

    using v-model for two way data binding

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
 new Vue({
     el:'#vue-app',
     data:{
-        // age:25,
-        // x:0,
-        // y:0
+        name: '',
+        age: ''
     },
     methods:{
         logName: function() {
@@ -12,19 +11,5 @@ new Vue({
         logAge: function() {
             console.log('Enter age');
         }
-        // add : function(inc) {
-        //     this.age+=inc;
-        // },
-        // subtract : function(dec) {
-        //     this.age-=dec;
-        // },
-        // updateXY : function(event) {
-        //     // console.log(event)
-        //     this.x = event.offsetX;
-        //     this.y = event.offsetY;
-        // },
-        // click: function() {
-        //     alert('You click me');
-        // }
     }
 });
