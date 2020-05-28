d8fe1454605c6448d22b13276cf19bc771f91ab9
anower00 <anower.hasan@gmail.com>
2020-05-28 21:12:50 +0600

    dynamic css classes

--- app.js
+++ app.js
@@ -1,28 +1,18 @@
 new Vue({
     el:'#vue-app',
     data:{
-        age: 25,
-        a:0,
-        b:0
+        available: false,
+        nearby: false,
     },
     methods:{
-        // addToA: function() {
-        //     console.log('Add to A');
-        //     return this.a + this.age;
-        // },
-        // addToB: function() {
-        //     console.log('Add to B');
-        //     return this.b + this.age;
-        // }
+     
     },
     computed:{
-        addToA: function() {
-            console.log('Add to A');
-            return this.a + this.age;
-        },
-        addToB: function() {
-            console.log('Add to B');
-            return this.b + this.age;
-        }
+       compClasses: function(){
+           return{
+               available: this.available,
+               nearby: this.nearby
+           }
+       }
     }
 });
