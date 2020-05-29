65b926952abefc050e8a8259bdc46af685fd82c6
anower00 <anower.hasan@gmail.com>
2020-05-29 22:03:05 +0600

    vue cli components

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>{{ title }}</h1>
+    <ninjas></ninjas>  
+  </div>
+</template>
+
+<script>
+import Ninja from './Ninja.vue'
+
+export default {
+  components:{
+    'ninjas':Ninja
+  },
+  data () {
+    return {
+     title: 'Ninja App'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
